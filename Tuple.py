cd14eab3c0e5be8fbe7b2cfea65485ae927857af
Muhammad77-star <hussainizyaan@gmail.com>
2024-10-11 16:43:12 +0400

    weather

---
+++ Tuple.py
@@ -0,0 +1,11 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+tuplex = (6, 3, 2, 8, 3, 1)
+print(tuplex)
+tuplex = tuplex + (9,)
+print(tuplex)
+tuple = (2, 3, 6, 9, 1, 5, 8)
+_slice = tuplex[3:5]
+print(_slice)
+_slice = tuplex[:2]
+print(_slice)
